32bf371247a971464d0b4302b2a6da8f803ca025
Russell Gordon <rgordon@lcs.on.ca>
2023-02-02 11:14:19 -0500

    Initial Commit

---
+++ CarTradingCards/CarTradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarTradingCardsApp.swift
+//  CarTradingCards
+//
+//  Created by Russell Gordon on 2023-02-02.
+//
+
+import SwiftUI
+
+@main
+struct CarTradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
