81cfe17c8eafb6f94d8f2e0b09c3d7b11ef47ea1
Russell Gordon <rgordon@lcs.on.ca>
2023-02-02 11:33:47 -0500

    Added image of car and re-organized project.

---
+++ CarTradingCards/Views/CarDetailView.swift
@@ -0,0 +1,22 @@
+//
+//  CarDetailView.swift
+//  CarTradingCards
+//
+//  Created by Russell Gordon on 2023-02-02.
+//
+
+import SwiftUI
+
+struct CarDetailView: View {
+    var body: some View {
+        Image("porsche")
+            .resizable()
+            .scaledToFit()ç
+    }
+}
+
+struct CarDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        CarDetailView()
+    }
+}
